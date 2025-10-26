4110ec9734314bd15f8004f49978ea584260e46e
Iconic-PC <ndukwegoodluck18@gmail.com>
2025-10-26 19:36:56 +0100

    Fix Vite config and dependencies for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
-    port: 5173, // default Vite port
+    port: 8080,
   },
-  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist", // Netlify picks up the "dist" folder by default
   },
 });
